46a7478e9bcb5e5e3bc9c454cf85975788117ac7
AryanGh-imp <aryan.ghasemi.imp@gmail.com>
2025-06-11 05:13:45 +0330

    feat(ui): add animation to the phoneNumber page

--- src/main/java/Client/Controllers/PhoneNumberController.java
+++ src/main/java/Client/Controllers/PhoneNumberController.java
@@ -1,11 +1,17 @@
 package Client.Controllers;
 
+import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+import javafx.util.converter.NumberStringConverter;
+
+import static Shared.Utils.SceneUtil.changeSceneWithSameSize;
 
 public class PhoneNumberController {
     @FXML
@@ -23,6 +29,17 @@ public class PhoneNumberController {
     );
 
     public void initialize() {
+
+        // Animation of moving from a little further right to the main target
+        if (root != null) {
+            root.setTranslateX(75);
+            TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
+            transition.setToX(0);
+            transition.setAutoReverse(false);
+            transition.setCycleCount(1);
+            transition.play();
+        }
+
         System.out.println("Initializing PhoneNumberController...");
         if (countryCode != null) {
             countryCode.setItems(countryCodes);
@@ -31,6 +48,14 @@ public class PhoneNumberController {
         } else {
             System.out.println("countryCode is null! Check FXML binding.");
         }
+        // Limit input to numbers and a maximum of 12 digits (for local numbers)
+        phoneNumberField.setTextFormatter(new TextFormatter<>(new NumberStringConverter(), null, change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d*") && newText.length() <= 12) { // Maximum 12 digits
+                return change;
+            }
+            return null; // Reject non-numeric changes or more than 12 digits
+        }));
     }
 
     @FXML
@@ -44,6 +69,11 @@ public class PhoneNumberController {
         }
 
         System.out.println("Phone Number: " + selectedCountryCode + " " + phoneNumber);
+        // TODO:
         // go to the next scene
+        // Set condition
+        changeSceneWithSameSize(root, "/Client/fxml/verificationViaTelegram.fxml");
+        // else (for new account)
+        // changeSceneWithSameSize(root, "/Client/fxml/verificationViaSms.fxml");
     }
 }
